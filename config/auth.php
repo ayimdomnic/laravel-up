9ad25529f2e856721e8cc4fc021fa5cb0fe0226c
Status CTO <ayimdomnic@statuscreative.com>
2016-10-12 00:14:41 -0700

    Admin Middleware exception resolved

--- config/auth.php
+++ config/auth.php
@@ -41,6 +41,16 @@ return [
             'provider' => 'users',
         ],
 
+        'user' => [
+            'driver' => 'session',
+            'provider' => 'users'
+        ],
+
+        'admin' => [
+            'driver' => 'session',
+            'provider' => 'admins'
+        ],
+
         'api' => [
             'driver' => 'token',
             'provider' => 'users',
@@ -69,6 +79,11 @@ return [
             'driver' => 'eloquent',
             'model' => App\User::class,
         ],
+
+        'admins' => [
+            'driver' => 'eloquent',
+            'model' => App\Admin::class,
+        ]
 
         // 'users' => [
         //     'driver' => 'database',
@@ -102,6 +117,11 @@ return [
             'table' => 'password_resets',
             'expire' => 60,
         ],
-    ],
+        'admins' => [
+            'provider' => 'admin',
+            'email' => 'admin.auth.emails.password',
+            'table' => 'password_resets',
+            'expire' => 60,
+        ],
 
 ];
