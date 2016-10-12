ac9c8af7beb5033e20786b572925d4f56927ae4e
Status CTO <ayimdomnic@statuscreative.com>
2016-10-12 00:22:57 -0700

    Frontend tweaks for the admin area

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -21,14 +21,14 @@ Route::get('/home', 'HomeController@index');
 
 Route::group(['middleware' => ['web']], function () {
     //Login Routes...
-    Route::get('/admin/login','AdminAuth\AuthController@showLoginForm');
-    Route::post('/admin/login','AdminAuth\AuthController@login');
-    Route::get('/admin/logout','AdminAuth\AuthController@logout');
+    Route::get('/admin/login','Admin\AuthController@showLoginForm');
+    Route::post('/admin/login','Admin\AuthController@login');
+    Route::get('/admin/logout','Admin\AuthController@logout');
     // Registration Routes...
-    Route::get('admin/register', 'AdminAuth\AuthController@showRegistrationForm');
-    Route::post('admin/register', 'AdminAuth\AuthController@register');
-    Route::post('admin/password/email','AdminAuth\PasswordController@sendResetLinkEmail');
-    Route::post('admin/password/reset','AdminAuth\PasswordController@reset');
-    Route::get('admin/password/reset/{token?}','AdminAuth\PasswordController@showResetForm');
-    Route::get('/admin', 'AdminController@index');
+    Route::get('admin/register', 'Admin\AuthController@showRegistrationForm');
+    Route::post('admin/register', 'Admin\AuthController@register');
+    Route::post('admin/password/email','Admin\PasswordController@sendResetLinkEmail');
+    Route::post('admin/password/reset','Admin\PasswordController@reset');
+    Route::get('admin/password/reset/{token?}','Admin\PasswordController@showResetForm');
+    Route::get('/admin', 'Backend\AdminController@index');
 });  
